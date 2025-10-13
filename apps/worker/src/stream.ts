6c78060234727b6aa291cfcdaecb874964b3882d
jujutsu-29 <rajtilakpandey16@gmail.com>
2025-10-13 23:28:02 +0530

    tsconfig file for packages/db

--- apps/worker/src/stream.ts
+++ apps/worker/src/stream.ts
@@ -63,7 +63,7 @@ import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const s3 = new S3Client({
-  region: process.env.S3_REGION!,
+  region: process.env.AWS_REGION!,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
